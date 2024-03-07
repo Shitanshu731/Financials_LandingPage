1a3a428c27c52144593f95baafbb90193e3b2449
Shitcoder731 <shitanshu.731@gmail.com>
2024-03-07 17:06:01 +0530

    Feature : Blur Navbar's Background

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from '../../constants/utils'
+import styles from "../styles/Navbar.module.css"
 
 const Navbar = () => {
   return (
-    <div className = "fixed z-[999]  w-full px-20 py-8 flex justify-between items-center">
+    <div className = {`${styles.navbar} fixed z-[999]  w-full px-20 py-8 flex justify-between items-center shadow-lg bg-blend-multiply` }>
         <div className='logo'>
-            <img src ="https://pssfinancial.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpss_logo.6d80ab5c.png&w=640&q=75" width = "200rem"/>
+            <img src ="https://pssfinancial.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpss_logo.6d80ab5c.png&w=640&q=75" width = "290rem"/>
         </div>
         <div className="links flex gap-10">
             {Link.map((i,index) =>(
