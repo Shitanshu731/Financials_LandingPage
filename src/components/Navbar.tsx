a66762f5bf883a3eb39fc2f1f0cd2b2dc2b5d466
Shitcoder731 <shitanshu.731@gmail.com>
2024-03-08 19:28:04 +0530

    Fix : MobileView of Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,18 +6,25 @@ import Image from "next/image";
 const Navbar: React.FC = () => {
   return (
     <div
-      className={`${styles.navbar} fixed z-[999]  w-full px-20 py-8 flex justify-between items-center shadow-lg bg-blend-multiply `}
+      className={`${styles.navbar} fixed z-[999]  w-full px-20 py-8 flex justify-between max-md:gap-6 items-center md:shadow-lg bg-blend-multiply`}
     >
-      <div className="logo">
+      <div className="logo max-md:hidden">
         <Image src={"/pss_logo.png"} alt="PSS_Logo" width={200} height={200} />
       </div>
-      <div className={`${styles.links} flex gap-10 max-md:hidden`}>
+      <div className={`${styles.links} flex gap-10 max-md:gap-0 max-md:w-full`}>
         {Link.map((i, index) => (
           <a
             key={index}
             className={`${
+              index === 1  ?
+                "max-md:hidden"  :""  
+            } 
+            ${
+              index === 0  ?
+                "max-md:hidden"  :""  
+            } ${
               index === 2
-                ? "ml-28 bg-[#FFBE55] rounded-full p-2 flex items-center gap-3"
+                ? "ml-28 bg-[#FFBE55] rounded-full p-2 flex items-center gap-3 max-md:p-4 max-md:max-h-10 max-md:text-[10px] max-md:w-52 max-md:mx-auto justify-between"
                 : ""
             } text-xl font-semibold cursor-pointer`}
           >
