fa121809c9919d08c7768dca67b7b1ad12c05672
Shitcoder731 <shitanshu.731@gmail.com>
2024-03-08 20:29:36 +0530

    Fix : MobileView of TitlePage

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -7,11 +7,11 @@ import styles from "../styles/LandingPage.module.css"
 const LandingPage = () => {
   return (
     <div className={`${styles.landingMain} w-full h-screen pt-1 font-bold`}>
-      <div className="textstructure mt-52 flex flex-col px-20">
+      <div className="textstructure mt-52 flex flex-col px-20 max-md:mt-72">
         {quotes.map((i, index) => (
-          <div key={index} className="Quotes">
+          <div key={index} className="Quotes ">
             <div className="w-fit flex items-center overflow-hidden">
-                {index === 1 && (<MotionDiv initial={{width : 0,opacity : 0}} animate ={{width : "14vw",opacity : 1}} transition={{ease : [0.56,0,0.24,1]}} className="mr-3 ml-3 w-[10vw] h-[6vw] top-[.3vw] relative "><Image src ={"/Investement.jpg"} alt="Investement_Logo"  width={600} height={300} /></MotionDiv>)}
+                {index === 1 && (<MotionDiv initial={{width : 0,opacity : 0}} animate ={{width : "14vw",opacity : 1}} transition={{ease : [0.56,0,0.24,1]}} className="mr-3 ml-3 w-[10vw] h-[6vw] top-[.3vw] relative "><Image src ={"/gif.gif"} alt="Investement_Logo"  width={600} height={300} /></MotionDiv>)}
               <h1 className="uppercase text-[#25291C] text-[9vw] leading-[.79] tracking-tighter font-medium ">
                 {i}
               </h1>
--- src/components/TitlePage.tsx
+++ src/components/TitlePage.tsx
@@ -3,8 +3,8 @@ import { Motionh1 } from './MotionDiv';
 import styles from "../styles/TitlePage.module.css"
 const TitlePage = () => {
   return (
-    <div className={` ${styles.titleBack} w-full py-20 rounded-tl-3xl rounded-tr-3xl`}>
-      <div className={`${styles.title} text border-t-2 border-b-2 border-[#FDF7FA] flex overflow-hidden`} >
+    <div className={` ${styles.titleBack} w-full py-20 rounded-tl-3xl rounded-tr-3xl max-md:absolute max-md:top-6`}>
+      <div className={`${styles.title} text border-t-2 border-b-2  flex overflow-hidden max-md:bg-transparent`} >
         <Motionh1 initial={{x : "0"}} animate={{x: "-100%"}} transition={{repeat : Infinity, ease :"linear",duration: 10}} className={`${styles.titleText} text-[10vw] py-10 text-nowrap leading-none font-bold pr-10`}>
           Pss Financials 
         </Motionh1>
